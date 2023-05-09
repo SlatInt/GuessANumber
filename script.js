e1aaa49671d14a2a55c35e8db203a603a5f98787
SlatInt <114303671+SlatInt@users.noreply.github.com>
2023-05-09 16:08:07 +0200

    Refactoring the js code
    Reduced repeated code, introduced new functions and variables for clarity.

--- script.js
+++ script.js
@@ -3,17 +3,19 @@
 //Variables selecting elements
 const againBtn = document.querySelector(".again");
 const checkBtn = document.querySelector(".check");
-const randomNrPlaceholder = document.querySelector(".number");
+const numberBox = document.querySelector(".number");
 const scoreFld = document.querySelector(".score");
 const guessInput = document.querySelector(".guess");
 const highscoreFld = document.querySelector(".highscore");
 const statusMessage = document.querySelector(".message");
-const numbersRangePar = document.querySelector('.between');
+const numbersRangePar = document.querySelector(".between");
 
 //Other variables
 let mysteryNumber = Number;
-const startingScore = '10';
+const startingScore = 10;
 const ranges = [1, 20];
+const pointsUp = 4;
+const pointsDown = -1;
 
 //Setting starting number of points to 10
 scoreFld.textContent = startingScore;
@@ -26,8 +28,40 @@ const genRandomNumber = function (min, max) {
   min = Math.ceil(min);
   max = Math.ceil(max);
   mysteryNumber = Math.floor(Math.random() * (max - min + 1) + min);
-  console.log(mysteryNumber);
+  // console.log(mysteryNumber);
   return mysteryNumber;
+};
+
+//Value/style changing functions (F)
+//F to toggle target button visibility
+const toggleBtn = function (btnName) {
+  btnName.classList.toggle("turnedoff");
+};
+
+//F changing input border colors
+const colorBorders = function (newColor) {
+  guessInput.style.border = `4px solid ${newColor}`;
+};
+
+//F change text content of "mystery number box"
+const modNumberBox = function (boxTxt) {
+  numberBox.textContent = boxTxt;
+};
+
+//F changing text content of .message
+const modMessage = function (message) {
+  statusMessage.textContent = message;
+};
+
+//Fs modifying the score
+const modScore = function (points) {
+  scoreFld.textContent = Number(scoreFld.textContent) + points;
+};
+//F checking: update the highscore if score has higher value
+const highscoreUpdate = function () {
+  if (Number(scoreFld.textContent) > Number(highscoreFld.textContent)) {
+    highscoreFld.textContent = Number(scoreFld.textContent);
+  }
 };
 
 //Generate random number (mysteryNumber) on loading the page
@@ -36,41 +70,43 @@ genRandomNumber(ranges[0], ranges[1]);
 //On clicking Again
 againBtn.addEventListener("click", () => {
   genRandomNumber(ranges[0], ranges[1]),
-    (statusMessage.textContent = `Start guessing...!`),
+    modMessage(`Start guessing...!`),
     (guessInput.value = null),
-    (randomNrPlaceholder.textContent = `?`);
-  checkBtn.classList.toggle("turnedoff");
-  againBtn.classList.toggle("turnedoff");
-  guessInput.style.border = "4px solid #eee";
+    modNumberBox(`?`);
+  toggleBtn(checkBtn);
+  toggleBtn(againBtn);
+  colorBorders("#eee");
 });
 
 //On clicking Check
 checkBtn.addEventListener("click", () => {
-  const guessValue = Number(guessInput.value);
+  colorBorders("#eee");
+  const guessValue = guessInput.value;
   // the number is correctly guessed
   if (guessValue == mysteryNumber) {
-    statusMessage.textContent = `Bullseye!`;
-    guessInput.style.border = "4px solid #60b347";
-    randomNrPlaceholder.textContent = mysteryNumber;
-    scoreFld.textContent = Number(scoreFld.textContent) + 4;
-    Number(scoreFld.textContent) > Number(highscoreFld.textContent)
-      ? (highscoreFld.textContent = Number(scoreFld.textContent))
-      : (highscoreFld.textContent = Number(highscoreFld.textContent));
-      checkBtn.classList.toggle("turnedoff");
-      againBtn.classList.toggle("turnedoff");
-  } 
+    modMessage(`Bullseye!`);
+    colorBorders("#60b347");
+    modNumberBox(`${mysteryNumber}`);
+    modScore(pointsUp);
+    highscoreUpdate();
+    toggleBtn(checkBtn);
+    toggleBtn(againBtn);
+  }
   //no number is given in the input field
   else if (guessValue == "") {
-    statusMessage.textContent = `Choose a number first!`;
-    guessInput.style.border = "4px solid #cc2020";
-  } 
+    modMessage(`Choose a number first!`);
+    colorBorders("#cc2020");
+  }
   //missed the number; either too high or too low
   else {
-    guessValue < mysteryNumber
-      ? (statusMessage.textContent = `Nope, try higher.`)
-      : (statusMessage.textContent = `Nope, try lower.`);
+    if (guessValue < mysteryNumber) {
+      modMessage(`Nope, try higher.`);
+    } else {
+      modMessage(`Nope, try lower.`);
+    }
     Number(scoreFld.textContent) > 0
-      ? (scoreFld.textContent = Number(scoreFld.textContent) - 1)
-      : (scoreFld.textContent = Number(scoreFld.textContent));
+      ? modScore(pointsDown)
+      : (modMessage(`Oops, you're out of points! Take 10.`),
+        modScore(startingScore));
   }
 });
